ab185020c2ca862314bc0ce1fa017d200eaa33f1
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-08-28 23:24:45 +0530

    statusbar colour mismatch error solved

--- lib/pages/main_home.dart
+++ lib/pages/main_home.dart
@@ -34,74 +34,76 @@ class _MainHomeState extends State<MainHome> {
 
     getDetails(widget.args);
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const SizedBox(
-          height: 30,
-        ),
-        ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(img1),
-            radius: 30,
+        body: SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 30,
           ),
-          title: Text(
-            'Welcome Back 👋',
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(img1),
+              radius: 30,
+            ),
+            title: Text(
+              'Welcome Back 👋',
+              style: TextStyle(
+                color: AppTheme.lightTheme(context).primaryColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            subtitle: Text(name),
+            trailing: const Text('224'),
+          ),
+          const SizedBox(
+            height: 70,
+          ),
+          const Text(
+            'Welcome to',
             style: TextStyle(
-              color: AppTheme.lightTheme(context).primaryColor,
+              fontSize: 30,
               fontWeight: FontWeight.bold,
-              fontSize: 18,
             ),
           ),
-          subtitle: Text(name),
-          trailing: const Text('224'),
-        ),
-        const SizedBox(
-          height: 70,
-        ),
-        const Text(
-          'Welcome to',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+          const SizedBox(
+            height: 10,
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          'Medorant',
-          style: TextStyle(
-            fontSize: 55,
-            fontWeight: FontWeight.bold,
-            color: AppTheme.lightTheme(context).primaryColor,
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.all(30),
-          child: Text(
-            'A simple yet powerful mobile app that tells you which \n Medicine is counterfeit for you.',
-            textAlign: TextAlign.center,
+          Text(
+            'Medorant',
             style: TextStyle(
-              fontSize: 13,
-              color: Color.fromARGB(255, 120, 120, 120),
+              fontSize: 55,
+              fontWeight: FontWeight.bold,
+              color: AppTheme.lightTheme(context).primaryColor,
             ),
           ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        InkWell(
-          child: Image.asset('assets/images/Search.png'),
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                  builder: (context) => const Scan(),
-                  settings: RouteSettings(arguments: widget.args)),
-            );
-          },
-        )
-      ],
+          const Padding(
+            padding: EdgeInsets.all(30),
+            child: Text(
+              'A simple yet powerful mobile app that tells you which \n Medicine is counterfeit for you.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 13,
+                color: Color.fromARGB(255, 120, 120, 120),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            child: Image.asset('assets/images/Search.png'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (context) => const Scan(),
+                    settings: RouteSettings(arguments: widget.args)),
+              );
+            },
+          )
+        ],
+      ),
     ));
   }
 }
--- lib/pages/text_scan.dart
+++ lib/pages/text_scan.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 // import 'package:google_mlkit_entity_extraction/google_mlkit_entity_extraction.dart';
 import 'package:image_picker/image_picker.dart';
@@ -53,127 +54,137 @@ class _ScanState extends State<Scan> {
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context)!.settings.arguments as String;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Scan Image'),
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: Colors.white, // <-- SEE HERE
+          statusBarIconBrightness:
+              Brightness.dark, //<-- For Android SEE HERE (dark icons)
+          statusBarBrightness:
+              Brightness.light, //<-- For iOS SEE HERE (dark icons)
         ),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              margin: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  if (textScanning) const CircularProgressIndicator(),
-                  if (!textScanning && imageFile == null)
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey[300]!,
-                      ),
-                      width: 300,
-                      height: 300,
+        centerTitle: true,
+        title: const Text(
+          'Scan Image',
+          style: TextStyle(color: Color(0xFF706FE5), fontSize: 26),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (textScanning) const CircularProgressIndicator(),
+                if (!textScanning && imageFile == null)
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[300]!,
                     ),
-                  const SizedBox(
-                    height: 30,
+                    width: 300,
+                    height: 300,
                   ),
-                  if (imageFile != null) Image.file(File(imageFile!.path)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        padding: const EdgeInsets.only(top: 10),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.grey,
-                            shadowColor: Colors.grey[400],
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0)),
-                          ),
-                          onPressed: () {
-                            getImage(ImageSource.gallery);
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(
-                                vertical: 5, horizontal: 5),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Icon(
-                                  Icons.image,
-                                  size: 30,
-                                ),
-                                Text(
-                                  "Gallery",
-                                  style: TextStyle(
-                                      fontSize: 13, color: Colors.grey[600]),
-                                )
-                              ],
-                            ),
+                const SizedBox(
+                  height: 30,
+                ),
+                if (imageFile != null) Image.file(File(imageFile!.path)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      padding: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.grey,
+                          shadowColor: Colors.grey[400],
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                        ),
+                        onPressed: () {
+                          getImage(ImageSource.gallery);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 5),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.image,
+                                size: 30,
+                              ),
+                              Text(
+                                "Gallery",
+                                style: TextStyle(
+                                    fontSize: 13, color: Colors.grey[600]),
+                              )
+                            ],
                           ),
                         ),
                       ),
-                      const SizedBox(
-                        width: 40,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        padding: const EdgeInsets.only(top: 10),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.grey,
-                            shadowColor: Colors.grey[400],
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8.0)),
-                          ),
-                          onPressed: () {
-                            getImage(ImageSource.camera);
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(
-                                vertical: 5, horizontal: 5),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Icon(
-                                  Icons.camera_alt,
-                                  size: 30,
-                                ),
-                                Text(
-                                  "Camera",
-                                  style: TextStyle(
-                                      fontSize: 13, color: Colors.grey[600]),
-                                )
-                              ],
-                            ),
+                    ),
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      padding: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.grey,
+                          shadowColor: Colors.grey[400],
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                        ),
+                        onPressed: () {
+                          getImage(ImageSource.camera);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 5),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.camera_alt,
+                                size: 30,
+                              ),
+                              Text(
+                                "Camera",
+                                style: TextStyle(
+                                    fontSize: 13, color: Colors.grey[600]),
+                              )
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => const MedicineDetails(),
-                            settings:
-                                RouteSettings(arguments: [scannedText, id]),
-                          ),
-                        );
-                        // settings: RouteSettings(arguments: scannedText)));
-                      },
-                      child: const Text('Get Data'))
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const MedicineDetails(),
+                          settings: RouteSettings(arguments: [scannedText, id]),
+                        ),
+                      );
+                      // settings: RouteSettings(arguments: scannedText)));
+                    },
+                    child: const Text('Get Data'))
+              ],
             ),
           ),
         ),
